b44ddeac03539123daf8e1c927e8983903d051c3
AnkitaCodder <119393582+AnkitaCodder@users.noreply.github.com>
2025-08-18 23:08:08 +0530

    Update pipelinecicd.groovy

--- vars/pipelinecicd.groovy
+++ vars/pipelinecicd.groovy
@@ -3,3 +3,13 @@ def gitdownload(repo)
    git branch: 'main', url: "https://github.com/AnkitaCodder/${repo}.git" 
 }
 
+def build()
+{
+  sh '''
+        python3 -m venv venv
+        source venv/bin/activate
+        pip install --upgrade pip
+        pip install -r requirements.txt
+    '''
+}
+
